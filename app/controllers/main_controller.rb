ac4615b3682f46aee17ca5bee362489c875857f3
Dwayne Macgowan <dwaynemac@gmail.com>
2022-10-08 17:14:42 -0300

    flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,11 @@
 class MainController < ApplicationController
   def index
   end
+
+  def test_flash
+    flash.now[:notice] = "hello"
+    flash.now[:alert] = "world"
+    index
+    render action: :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "test_flash", to: "main#test_flash"
+
   # Defines the root path route ("/")
   root to: "main#index"
 end
